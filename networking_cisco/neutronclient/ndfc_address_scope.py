62da5b2626039eecbacd76c5954d09f0c55412a3
Sayali Naval <sayalinaval@gmail.com>
2026-03-07 06:51:53 +0000

    Add nd-vrf-name address-scope extension and VRF lifecycle
    Implement ND-specific support for the nd-vrf-name attribute on Neutron
    address scopes and wire it into VRF lifecycle management:
    
    - Expose nd-vrf-name via an API extension and ML2 extension driver
    - Persist nd_vrf_name in a dedicated nd_address_scope_extensions table with
      a proper FK/relationship to address_scopes
    - Enforce per-IP-family uniqueness for a given nd-vrf-name
    - Refactor NdMl2Plugin.create_address_scope to follow the ML2/GBP pattern
      with a single DB transaction and mech driver pre/postcommit hooks
    - Add NdMl2Plugin.delete_address_scope cleanup that best-effort deletes
      the associated VRF in ND
    - Provide a shared get_ndfc_conf() helper for extension/manager usage
    - Add OSC and neutronclient support for --nd-vrf-name on create/show,
      and forbid combining --nd-vrf-name with --share
    - Add unit tests for the extension driver, plugin, and OSC behavior
    
    Change-Id: I68f1b7556f7f80a156d53ca4fabfc311f694561a
    (cherry picked from commit 5a7bc462f45c8a976e1ece594bf6a2876af2fdec)

---
+++ networking_cisco/neutronclient/ndfc_address_scope.py
@@ -0,0 +1,83 @@
+# Copyright 2026 Cisco Systems, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from __future__ import print_function
+
+from networking_cisco._i18n import _
+
+from neutronclient.common import extension
+from neutronclient.neutron import v2_0 as neutronV20
+
+
+ADDRESS_SCOPE = 'address_scope'
+
+
+class NdAddressScope(extension.NeutronClientExtension):
+    resource = ADDRESS_SCOPE
+    resource_plural = 'address_scopes'
+    object_path = '/address-scopes'
+    resource_path = '/address-scopes/%s'
+    versions = ['2.0']
+    allow_names = True
+
+
+class NdAddressScopeCreate(extension.ClientExtensionCreate,
+                           NdAddressScope):
+    shell_command = 'nd-address-scope-create'
+
+    def add_known_arguments(self, parser):
+        parser.add_argument(
+            '--nd-vrf-name',
+            dest='nd_vrf_name',
+            help=_('ND VRF name for this address scope.')
+        )
+        parser.add_argument(
+            '--ip-version',
+            dest='ip_version',
+            type=int,
+            choices=[4, 6],
+            required=True,
+            help=_('IP version of this address scope (4 or 6).')
+        )
+        parser.add_argument(
+            '--share',
+            dest='shared',
+            action='store_true',
+            default=False,
+            help=_('Make the address scope shared.')
+        )
+        parser.add_argument(
+            'name',
+            help=_('Name of the address scope.')
+        )
+
+    def args2body(self, parsed_args):
+        if getattr(parsed_args, 'nd_vrf_name', None) and getattr(
+                parsed_args, 'shared', False):
+            raise SystemExit(_(
+                'nd-vrf-name cannot be combined with shared address ') + _(
+                    'scopes'))
+
+        body = {self.resource: {
+            'name': parsed_args.name,
+            'ip_version': parsed_args.ip_version,
+            'shared': parsed_args.shared,
+        }}
+
+        if getattr(parsed_args, 'nd_vrf_name', None):
+            body[self.resource]['nd-vrf-name'] = parsed_args.nd_name
+
+        neutronV20.update_dict(parsed_args, body[self.resource], [])
+        return body
---
+++ networking_cisco/plugins/ml2/nd_manager.py
@@ -0,0 +1,67 @@
+# Copyright 2026 Cisco Systems, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_log import log as logging
+
+from networking_cisco.ml2_drivers.ndfc import extension_driver as nd_ext
+from networking_cisco.ml2_drivers.ndfc.ndfc import get_ndfc_conf
+
+
+LOG = logging.getLogger(__name__)
+
+
+class NdManager(object):
+    """Coordinator for ND-specific ML2 behavior."""
+
+    def __init__(self):
+        self._ext_driver = nd_ext.NdExtensionDriver()
+        try:
+            self._ext_driver.initialize()
+        except Exception:
+            LOG.exception("Failed to initialize NdExtensionDriver")
+
+    def handle_address_scope_create(self, context, body, result):
+        if not hasattr(self, "_ext_driver"):
+            return
+        if not hasattr(self._ext_driver, "process_create_address_scope"):
+            return
+        self._ext_driver.process_create_address_scope(context, body, result)
+
+    def delete_vrf_for_address_scope(self, nd_name):
+        if not nd_name:
+            return
+        try:
+            ndfc = get_ndfc_conf()
+            deleted = ndfc.delete_vrf(nd_name)
+            if not deleted:
+                LOG.error(
+                    "Failed to delete VRF %s in ND for "
+                    "address-scope cleanup", nd_name,
+                )
+            else:
+                LOG.debug(
+                    "Deleted VRF %s in ND for address-scope cleanup",
+                    nd_name,
+                )
+        except Exception:
+            LOG.exception(
+                "Exception while deleting VRF %s in ND for "
+                "address-scope cleanup", nd_name,
+            )
+
+    def extend_address_scope(self, session, base_model, result):
+        if not hasattr(self, "_ext_driver"):
+            return
+        self._ext_driver.extend_address_scope_dict(session, base_model, result)
